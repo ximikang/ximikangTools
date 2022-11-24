c0a781eec9b910aae6dae6bc769718766b85581a
ximikang <ximikang@outlook.com>
2022-11-25 00:02:58 +0800

    :tada: init repo

---
+++ static/bash/rpi-replace-apt-source-buster.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+echo "start replace sources to tuna mirror."
+
+echo "replace /etc/apt/sources.list"
+cat << EOF > /etc/apt/sources.list
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-updates main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-updates main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-backports main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-backports main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian-security bullseye-security main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian-security bullseye-security main contrib non-free
+EOF
+
+echo "replace /etc/apt/sources.list.d/raspi.list"
+cat << EOF > /etc/apt/sources.list.d/raspi.list
+deb https://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ bullseye main
+EOF
+
+echo "replace to tuna mirror."
